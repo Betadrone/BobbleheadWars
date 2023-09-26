f573bc79c5cfcbef67e3b9bd8f3f15c4eaa3f941
Muhammad Ashhad <betadrone596@gmail.com>
2023-09-26 15:16:15 -0400

    Chapter_4 - Added 8 layers; adjusted colliders; programmed bobblehead characteristic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
     private CharacterController characterController;
+    public Rigidbody head;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +19,18 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if(moveDirection == Vector3.zero)
+        {
+            //ToDo
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
     }
 }
