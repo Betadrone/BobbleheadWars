f1ebcf8fe536611aec6ac97645ccb3ae49321ea2
Muhammad Ashhad <betadrone596@gmail.com>
2023-10-13 20:59:25 -0400

    Chapter_7 - powerup sound

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,11 +6,14 @@ public class Gun : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform launchPosition;
-
+    private AudioSource audioSource;
+    public bool isUpgraded;
+    public float upgradeTime = 10.0f;
+    private float currentTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,16 +30,54 @@ public class Gun : MonoBehaviour
         {
             CancelInvoke("fireBullet");
         }
+
+        currentTime += Time.deltaTime;
+        if(currentTime > upgradeTime && isUpgraded == true) 
+        {
+            isUpgraded = false;
+        }
     }
 
     //bullet code
     void fireBullet()
     {
         //1 - uses the bullet prefab to create an instance of it in the game.
-        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        //GameObject bullet = Instantiate(bulletPrefab) as GameObject;
         //2 - sets the spawn position of the created instance to the barrel of the gun.
-        bullet.transform.position = launchPosition.position;
+        //bullet.transform.position = launchPosition.position;
         //3 - gives the bullet a velocity heading towards whichever direction the object is facing whom this script is attached to.
-        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+        //bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+        Rigidbody bullet = createBullet();
+        bullet.velocity = transform.parent.forward * 100;
+
+        if(isUpgraded)
+        {
+            Rigidbody bullet2 = createBullet();
+            bullet2.velocity = (transform.right + transform.forward / 0.5f) * 100;
+            Rigidbody bullet3 = createBullet();
+            bullet3.velocity = ((transform.right * -1) + transform.forward / 0.5f) * 100;
+        }
+
+        if(isUpgraded)
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.upgradedGunFire); // this plays shooting sound
+        }
+        else
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.gunFire); // this plays shooting sound
+        }
+    }
+
+    private Rigidbody createBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        bullet.transform.position = launchPosition.position;
+        return bullet.GetComponent<Rigidbody>();
+    }
+
+    public void UpgradeGun()
+    {
+        isUpgraded = true;
+        currentTime = 0;
     }
 }
